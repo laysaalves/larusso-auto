73753c59510f5a7ac08a63ac47995a7308149763
Layseiras <layseirasdev@outlook.com>
2025-02-03 09:51:08 -0300

    first commit

---
+++ src/test/java/dev/layseiras/larussocars/LarussocarsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.larussocars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarussocarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
